838b73b4c3889077fef11a526a4d426b49539071
Santiago Pavon <santiago@dit.upm.es>
2016-03-04 11:23:38 +0100

    Tema 4: No perder respuesta al reintentar.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,20 @@
 
 // GET /question
 exports.question = function(req, res, next) {
-   res.render('quizzes/question', {question: 'Capital de Italia'});
+
+	var answer = req.query.answer || '';
+
+	res.render('quizzes/question', {question: 'Capital de Italia',
+								    answer: answer});
 };
 
 // GET /check
 exports.check = function(req, res, next) {
-	var result = req.query.answer === 'Roma' ? 'Correcta' : 'Incorrecta';
-	res.render('quizzes/result', {result: result});
+
+	var answer = req.query.answer || "";
+
+	var result = answer === 'Roma' ? 'Correcta' : 'Incorrecta';
+
+	res.render('quizzes/result', { result: result, 
+								   answer: answer });
 };
