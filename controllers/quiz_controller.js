748f343724aaa3373b99bcea03b80bfbd1dfb7d7
Santiago Pavon <santiago@dit.upm.es>
2016-03-04 11:49:16 +0100

    Tema 16: Borrar Preguntas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -120,7 +120,18 @@ exports.update = function(req, res, next) {
 };
 
 
-
+// DELETE /quizzes/:id
+exports.destroy = function(req, res, next) {
+  req.quiz.destroy()
+    .then( function() {
+	  req.flash('success', 'Quiz borrado con éxito.');
+      res.redirect('/quizzes');
+    })
+    .catch(function(error){
+	  req.flash('error', 'Error al editar el Quiz: '+error.message);
+      next(error);
+    });
+};
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get('/quizzes/new',                 quizController.new);
 router.post('/quizzes',                    quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',  quizController.edit);
 router.put('/quizzes/:quizId(\\d+)',       quizController.update);
+router.delete('/quizzes/:quizId(\\d+)',    quizController.destroy);
 
 
 module.exports = router;
