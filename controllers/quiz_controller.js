1c0b41c35c14c03865f7f29307905ce4b35b4550
Santiago Pavon <santiago@dit.upm.es>
2016-03-04 11:46:25 +0100

    Tema 14: Validacion de Entradas.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,6 @@
 
 var models = require('../models');
+var Sequelize = require('sequelize');
 
 // Autoload el quiz asociado a :quizId
 exports.load = function(req, res, next, quizId) {
@@ -67,6 +68,15 @@ exports.create = function(req, res, next) {
   	.then(function(quiz) {
 		req.flash('success', 'Quiz creado con éxito.');
     	res.redirect('/quizzes');  // res.redirect: Redirección HTTP a lista de preguntas
+    })
+    .catch(Sequelize.ValidationError, function(error) {
+
+      req.flash('error', 'Errores en el formulario:');
+      for (var i in error.errors) {
+          req.flash('error', error.errors[i].value);
+      };
+
+      res.render('quizzes/new', {quiz: quiz});
     })
     .catch(function(error) {
 		req.flash('error', 'Error al crear un Quiz: '+error.message);
--- models/quiz.js
+++ models/quiz.js
@@ -3,7 +3,11 @@
 
 module.exports = function(sequelize, DataTypes) {
   return sequelize.define('Quiz',
-                          { question: DataTypes.STRING,
-                            answer:   DataTypes.STRING
+                          { question: { type: DataTypes.STRING,
+                          	            validate: { notEmpty: {msg: "Falta Pregunta"}}
+                          	          },
+                            answer:   { type: DataTypes.STRING,
+                                        validate: { notEmpty: {msg: "Falta Respuesta"}}
+                                      }
                           });
 };
