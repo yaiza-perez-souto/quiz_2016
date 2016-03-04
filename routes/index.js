600db764e059764c1daa749dc02e906c5bd17c0d
Santiago Pavon <santiago@dit.upm.es>
2016-03-04 11:27:59 +0100

    Tema 5: Marco de Aplicacion.

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index');
 });
 
 router.get('/question', quizController.question);
