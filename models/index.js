deccf43bdaee755bd0957d303bd99bdbbd5dccb0
Santiago Pavon <santiago@dit.upm.es>
2016-04-25 11:08:15 +0200

    Si no existe $DATABASE_URL, se configura sqlite por defecto.

--- models/index.js
+++ models/index.js
@@ -8,14 +8,20 @@ var Sequelize = require('sequelize');
 //    DATABASE_URL = sqlite:///
 //    DATABASE_STORAGE = quiz.sqlite
 
-var url = process.env.DATABASE_URL;
-var storage = process.env.DATABASE_STORAGE || "";
+var url, storage;
+
+if (!process.env.DATABASE_URL) {
+    url = "sqlite:///";
+    storage = "quiz.sqlite";
+} else {
+    url = process.env.DATABASE_URL;
+    storage = process.env.DATABASE_STORAGE || "";
+}
 
 var sequelize = new Sequelize(url, 
 	 						  { storage: storage,
 				              	omitNull: true 
 				              });
-
 
 // Importar la definicion de la tabla Quiz de quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
