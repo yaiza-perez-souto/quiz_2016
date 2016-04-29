5749b46ba8b4ebef10455c6a19f95adf9003e9f7
Santiago Pavon <santiago@dit.upm.es>
2016-04-29 17:46:53 +0200

    Al autenticar, diferenciar entre errores y fallos de autenticacion.

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -16,16 +16,20 @@ exports.create = function(req, res, next) {
 
     userController.authenticate(login, password)
         .then(function(user) {
+            if (user) {
+    	        // Crear req.session.user y guardar campos id y username
+    	        // La sesión se define por la existencia de: req.session.user
+    	        req.session.user = {id:user.id, username:user.username};
 
-	        // Crear req.session.user y guardar campos id y username
-	        // La sesión se define por la existencia de: req.session.user
-	        req.session.user = {id:user.id, username:user.username};
-
-	        res.redirect("/"); // redirección a la raiz
+    	        res.redirect("/"); // redirección a la raiz
+            } else {
+                req.flash('error', 'La autenticación ha fallado. Reinténtelo otra vez.');
+                res.redirect("/session"); // redirect a login
+            }
 		})
 		.catch(function(error) {
             req.flash('error', 'Se ha producido un error: ' + error);
-            res.redirect("/session");        
+            next(error);        
     });
 };
 
