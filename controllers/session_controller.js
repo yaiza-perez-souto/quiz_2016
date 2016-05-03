23e78e64e8488b870870fa06b8ff965827b39510
Santiago Pavon <santiago@dit.upm.es>
2016-05-03 11:38:46 +0200

    Eliminados los cambios para autenticar usando Passport-local."

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -2,36 +2,26 @@
 var models = require('../models');
 var Sequelize = require('sequelize');
 
-// Configurar Passport
-
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
 /*
  * Autenticar un usuario: Comprueba si el usuario esta registrado en users
  *
- * DBusca el usuario con el username dado y comprueba su password.
- * Si la autenticacion es correcta, llama a done(null, user).
- * Si la autenticacion falla, llama a done(null,false).
- * Si hay errores llama a done(error);
+ * Devuelve una Promesa que busca el usuario con el login dado y comprueba su password.
+ * Si la autenticacion es correcta, la promesa se satisface devuelve un objeto con el User.
+ * Si la autenticacion falla, la promesa se satisface pero devuelve null.
  */
-passport.use(new LocalStrategy(
-    function(username, password, done) {
+var authenticate = function(login, password) {
+    
+    return models.User.findOne({where: {username: login}})
+        .then(function(user) {
+            if (user && user.verifyPassword(password)) {
+                return user;
+            } else {
+                return null;
+            }
+        });
+}; 
 
-        models.User.findOne({where: {username: username}})
-            .then(function(user) {
-                if (user && user.verifyPassword(password)) {
-                    return done(null, user); 
-                } else {
-                    return done(null,false);        
-                } 
-            })
-            .catch(function(error) {
-                return done(error);        
-            });
-    }
-));
- 
 
 
 // GET /session   -- Formulario de login
@@ -43,24 +33,26 @@ exports.new = function(req, res, next) {
 // POST /session   -- Crear la sesion si usuario se autentica
 exports.create = function(req, res, next) {
 
-    passport.authenticate('local', function(error, user, info) {
-        if (error) { 
+    var login     = req.body.login;
+    var password  = req.body.password;
 
+    authenticate(login, password)
+        .then(function(user) {
+            if (user) {
+    	        // Crear req.session.user y guardar campos id y username
+    	        // La sesión se define por la existencia de: req.session.user
+    	        req.session.user = {id:user.id, username:user.username};
+
+    	        res.redirect("/"); // redirección a la raiz
+            } else {
+                req.flash('error', 'La autenticación ha fallado. Reinténtelo otra vez.');
+                res.redirect("/session"); // redirect a login
+            }
+		})
+		.catch(function(error) {
             req.flash('error', 'Se ha producido un error: ' + error);
-            return next(error); 
-        }
-
-        if (user) { 
-            // Crear req.session.user y guardar campos id y username
-            // La sesión se define por la existencia de: req.session.user
-            req.session.user = {id:user.id, username:user.username};
-
-            res.redirect("/"); // redirección a la raiz  
-        } else {
-            req.flash('error', 'La autenticación es incorrecta. Reinténtelo otra vez.');
-            res.redirect("/session"); 
-        }
-    })(req, res, next);
+            next(error);        
+    });
 };
 
 
